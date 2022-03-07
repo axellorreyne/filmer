f5180e81e14a4663d4beb5d730779f0770128527
Elias Nijs <elias.nijs@gmail.com>
2022-03-07 16:58:47 +0100

    minor fix to the landing page

--- frontend/src/landing_page/LandingPage.js
+++ frontend/src/landing_page/LandingPage.js
@@ -10,18 +10,15 @@ class LandingPage extends Component
 
   componentDidMount() {
     document.title = "Filmer: Where people find their favourite movies!";
-    document.icon = "";
   }
 
   render ()
   {
     return (
 <div class="cover-container d-flex h-100 p-3 mx-auto flex-column text-center">
-
   <header class="masthead mb-auto">
     <img src={RsrcLogo} width="100px"/>
   </header>
-
   <main role="main" class="mb-5">
     <h1 class="cover-heading">Where people find their favourite movies!</h1>
     <div class="mt-5">
@@ -29,16 +26,13 @@ class LandingPage extends Component
       <button type="button" class="btn btn-light m-1">Log in</button>
     </div>
   </main>
-
   <footer class="mastfoot mt-auto">
     <div class="inner">
       <p><a href="">Terms of Service</a> - <a href="">Privacy Policy</a> - <a href="">Cookie Policy</a> - <a href="">About</a> - <a href="">Developers</a></p>
       <p>(c) 2022 filmer inc.</p>
     </div>
   </footer>
-
 </div>
-  
     );
   }
 
