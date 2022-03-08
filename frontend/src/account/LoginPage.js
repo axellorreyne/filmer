83a23a075c04a38fa3128622dcc170a4604fe2a0
renevds <rene.v.d.s.9@gmail.com>
2022-03-08 15:23:48 +0100

    Router setup

---
+++ frontend/src/account/LoginPage.js
@@ -0,0 +1,11 @@
+import {Component} from "react";
+
+class LoginPage extends Component
+{
+
+    render() {
+        return("Login")
+    }
+}
+
+export default LoginPage;
--- frontend/src/landing_page/LandingPage.js
+++ frontend/src/landing_page/LandingPage.js
@@ -1,42 +1,44 @@
 import {Component} from "react";
 import axios from "axios";
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 
 import "./LandingPage.css";
-import Test from "../Test";
 
 import RsrcLogo from "../resources/logo_transparant.svg";
 
-class LandingPage extends Component 
-{
+class LandingPage extends Component {
 
-  componentDidMount() {
-    document.title = "Filmer: Where people find their favourite movies!";
-  }
+    componentDidMount() {
+        document.title = "Filmer: Where people find their favourite movies!";
+    }
 
-  render ()
-  {
-    return (
-<div class="cover-container d-flex h-100 p-3 mx-auto flex-column text-center">
-  <header class="masthead mb-auto">
-    <img src={RsrcLogo} width="100px"/>
-  </header>
-  <main role="main" class="mb-5">
-    <h1 class="cover-heading">Where people find their favourite movies!</h1>
-    <div class="mt-5">
-      <Link to="/createaccountpage"><button type="button" class="btn btn-light m-1">Sign up</button></Link>
-      <Link to="/test"><button type="button" class="btn btn-light m-1">Log in</button></Link>
-    </div>
-  </main>
-  <footer class="mastfoot mt-auto">
-    <div class="inner">
-      <p><a href="">Terms of Service</a> - <a href="">Privacy Policy</a> - <a href="">Cookie Policy</a> - <a href="">About</a> - <a href="">Developers</a></p>
-      <p>(c) 2022 filmer inc.</p>
-    </div>
-  </footer>
-</div>
-    );
-  }
+    render() {
+        return (
+            <div className="cover-container d-flex h-100 p-3 mx-auto flex-column text-center">
+                <header className="masthead mb-auto">
+                    <img src={RsrcLogo} width="100px"/>
+                </header>
+                <main role="main" className="mb-5">
+                    <h1 className="cover-heading">Where people find their favourite movies!</h1>
+                    <div className="mt-5">
+                        <Link to="/signup">
+                            <button type="button" className="btn btn-light m-1">Sign up</button>
+                        </Link>
+                        <Link to="/login">
+                            <button type="button" className="btn btn-light m-1">Log in</button>
+                        </Link>
+                    </div>
+                </main>
+                <footer className="mastfoot mt-auto">
+                    <div className="inner">
+                        <p><a href="">Terms of Service</a> - <a href="">Privacy Policy</a> - <a href="">Cookie
+                            Policy</a> - <a href="">About</a> - <a href="">Developers</a></p>
+                        <p>(c) 2022 filmer inc.</p>
+                    </div>
+                </footer>
+            </div>
+        );
+    }
 
 }
 
