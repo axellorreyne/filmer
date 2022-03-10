12993a10dfab028c2ee4576849de2a15b7d06ddb
renevds <rene.v.d.s.9@gmail.com>
2022-03-10 23:40:48 +0100

    Passw length error fix

--- frontend/src/account/SignupPage.js
+++ frontend/src/account/SignupPage.js
@@ -33,10 +33,10 @@ const vusername = value => {
   }
 };
 const vpassword = value => {
-  if (value.length < 6 || value.length > 40) {
+  if (value.length < 8 || value.length > 40) {
     return (
       <div className="alert alert-danger" role="alert">
-        The password must be between 6 and 40 characters.
+        The password must be between 8 and 40 characters.
       </div>
     );
   }
