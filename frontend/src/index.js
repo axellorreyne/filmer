88ce49a2d24f65ff4c4588cb4737406b647f567c
renevds <rene.v.d.s.9@gmail.com>
2022-03-10 21:40:36 +0100

    Authentication fronten

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,12 +8,16 @@ import './style.css';
 
 import LandingPage from './landing_page/LandingPage';
 import LoginPage from "./account/LoginPage";
+import SignupPage from "./account/SignupPage";
+import ProfilePage from "./account/ProfilePage";
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<LandingPage/>}/>
             <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/signup" element={<SignupPage/>}/>
+            <Route path="/profile" element={<ProfilePage/>}/>
         </Routes>
     </BrowserRouter>,
     document.getElementById('root')
---
+++ frontend/src/services/user.service.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+const API_URL = "/api/auth/";
+class UserService {
+  //describe api here
+}
+export default new UserService();
