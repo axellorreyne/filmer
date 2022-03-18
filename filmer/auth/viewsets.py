2d52d3f48cb8a39a87ad313fee899bfd21059f95
renevds <rene.v.d.s.9@gmail.com>
2022-03-18 22:20:11 +0100

    User patch

--- filmer/auth/viewsets.py
+++ filmer/auth/viewsets.py
@@ -1,15 +1,12 @@
 # core/auth/viewsets
-from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework import status, viewsets
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from filmer.auth.serializers import LoginSerializer, RegisterSerializer
-from filmer.models import Reaction
-from filmer.serializers import ReactionSerializer
 
 
 class LoginViewSet(ModelViewSet, TokenObtainPairView):
@@ -64,18 +61,3 @@ class RefreshViewSet(viewsets.ViewSet, TokenRefreshView):
             raise InvalidToken(e.args[0])
 
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
-
-
-class ReactionViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
-    serializer_class = ReactionSerializer
-
-    def get_queryset(self):
-        return Reaction.objects.filter(user=self.request.user)
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.save(user=request.user)
-        return Response(status=status.HTTP_201_CREATED)
-
--- filmer/viewsets.py
+++ filmer/viewsets.py
@@ -1,13 +1,15 @@
-from rest_framework import filters
+from rest_framework import filters, status
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from filmer.serializers import UserSerializer
+from filmer.models import Reaction
+from filmer.serializers import UserSerializer, ReactionSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    http_method_names = ['get']
+    http_method_names = ['get', 'patch']
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
     filter_backends = [filters.OrderingFilter]
@@ -15,6 +17,8 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         if self.request.user.is_superuser:
             return User.objects.all()
+        else:
+            return [self.request.user]
 
     def get_object(self):
         lookup_field_value = self.kwargs[self.lookup_field]
@@ -23,3 +27,18 @@ class UserViewSet(viewsets.ModelViewSet):
         self.check_object_permissions(self.request, obj)
 
         return obj
+
+
+class ReactionViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ReactionSerializer
+    http_method_names = ['post', 'get']
+
+    def get_queryset(self):
+        return Reaction.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save(user=request.user)
+        return Response(status=status.HTTP_201_CREATED)
