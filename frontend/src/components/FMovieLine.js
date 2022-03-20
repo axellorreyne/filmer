fb197955fb9bbd774c9fb9d04eff91cdb7c2eb4d
Bavo <bavo.verstraeten@ugent.be>
2022-03-20 15:58:09 +0100

    liking adds a movie, correct amount, and no movies text

--- frontend/src/components/FMovieLine.js
+++ frontend/src/components/FMovieLine.js
@@ -9,19 +9,22 @@ class FMovieLine extends Component
 
     render ()
     {
+        const name = this.props.movie.original_title
+        const tags = this.props.movie.genres.map(genre=>genre.name)
+        const score = this.props.movie.vote_average
         return (
             <div classNameName="fmovieline m-0 p-0">
                 <hr className="m-0 my-md-2 my-3"/>
                 <div classNameName="d-xxl-flex align-items-center justify-content-between">
-                    <div className="me-auto ffs-2 ffw-2 mb-md-2">{this.props.name}</div>
+                    <div className="me-auto ffs-2 ffw-2 mb-md-2">{name}</div>
                     <div className="d-sm-flex justify-content-between">
                         <div className="justify-content-left my-3 my-sm-0">
-                            <FTagList tags={this.props.tags}/>
+                            <FTagList tags={tags}/>
                         </div>
                         <div className="d-flex">
                             <div className="d-flex align-items-center me-3">
                                 <img src={RsrcIconStar} width="24px" className="me-2" alt=""/>
-                                {this.props.score}/10
+                                {score}/10
                             </div>
                             <img src={RsrcIconCheck} width="24px" className="me-2" alt=""/>
                         </div>
--- frontend/src/page_mymovies/MyMoviesPage.js
+++ frontend/src/page_mymovies/MyMoviesPage.js
@@ -4,6 +4,9 @@ import FFooter from "../components/FFooter.js";
 import FHeader from "../components/FHeader";
 import FMovieLine from "../components/FMovieLine";
 
+import MovieService from "../services/movie.service";
+import UserService from "../services/user.service";
+
 import RsrcSearchIcon from "../resources/icon_search.svg";
 
 class MyMoviesPage extends Component
@@ -11,28 +14,36 @@ class MyMoviesPage extends Component
 
   constructor(probs) {
     super(probs);
-    this.searchTerm=""
+    this.state = {movies:[]}
   }
   componentDidMount()
   {
-
+    UserService.getReactions().then((data)=>data.filter(movie=>! movie.seen && movie.like).forEach(
+        movie=>MovieService.getMovieInfo(movie.movie_id).then(
+            info=> {
+              if (info.error === undefined) {
+                this.setState((prev, _) => ({movies: prev.movies.concat([info])}))
+              }
+            }
+        )
+    ))
     document.title = "Filmer: Where people find their favourite movies!";
   }
 
   render ()
   {
-    const movie1 = "Interstellar by Christopher Nolan"
-    const score1 = "8.8"
-    const tags1 = ["Sci-fi","Adventure","Drama"]
-    const movie2 = "Inception by Christopher Nolan"
-    const score2= "8.7"
-    const tags2 = ["Sci-fi","Adventure","Action"]
-    const amount = 2
+    const amount = this.state.movies.length
+
+    const rendered = (amount === 0)?
+        <div className="container-fluid mt-5">
+          <h5 >No movies to show.</h5>
+          <h5>Like movies on the homepage to view them here!</h5>
+        </div>
+    : this.state.movies.map(ele=><FMovieLine movie={ele}/>)
 
     return (
       <div className="h-100 d-flex flex-column m-3 m-xxl-0">
         <FHeader/>
-        <label className="ffw-2 rgb-alert mx-3 text-center">Functionality not implemented yet!</label>
         <main className="mb-5 container-fluid">
           <div className="my-5 d-lg-flex justify-content-around align-items-center">
             <div className="col-lg-7 mx-sm-5 mb-5" >
@@ -54,8 +65,7 @@ class MyMoviesPage extends Component
                 <button className="btn btn-primary m-0 p-1 ffw-2 d-none d-sm-block">Add movie</button>
                 <button className="btn btn-primary m-0 p-1 ffw-2 d-sm-none w-100 my-3">Add movie</button>
               </div>
-              <FMovieLine score={score1} name={movie1} tags={tags1}/>
-              <FMovieLine score={score2} name={movie2} tags={tags2}/>
+              {rendered}
             </div>
           </div>
         </main>
