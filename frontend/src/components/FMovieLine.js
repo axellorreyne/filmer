b26191f8b4cd8283015a1a8dc21d8093fdab8b1d
Bavo <bavo.verstraeten@ugent.be>
2022-03-20 20:33:19 +0100

    small tweak to seen/not_seen checks on mymovies

--- frontend/src/components/FMovieLine.js
+++ frontend/src/components/FMovieLine.js
@@ -3,20 +3,33 @@ import FTagList from "./FTagList";
 
 import RsrcIconStar from "../resources/icon_star.svg"
 import RsrcIconCheck from "../resources/icon_check.svg"
+import RsrcArrowRight from "../resources/icon_arrow_right.svg"
 
 class FMovieLine extends Component
 {
+
+    setSeen(movie_id,seen){
+        //to be implemented
+
+
+    }
 
     render ()
     {
         const name = this.props.movie.original_title
         const tags = this.props.movie.genres.map(genre=>genre.name)
-        const score = this.props.movie.vote_average
+        const score = this.props.movie.vote_average.toFixed(1)
         const director = "by "+this.props.movie.credits.crew.filter(x=>x.job==="Director").slice(0,3)
             .map(x=>x.name).sort().join(", ")
-        const check = (this.props.seen)?
+        const inner =
+            (this.props.seen)?
             <img src={RsrcIconCheck} width="24px" className="me-2" alt=""/>
-            :<div />
+            :<img src={RsrcArrowRight} width="24px" className="me-2" alt=""/>
+
+        const check =
+            <button onClick={()=>this.setSeen(this.props.movie.id,!this.props.seen)} className="bg-transparent border-0">
+                {inner}
+            </button>
 
         return (
             <div classNameName="fmovieline m-0 p-0">
