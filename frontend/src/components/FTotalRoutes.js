c29ea4550d9390b39f5a6f3977a34a1d1d189010
Bavo <bavo.verstraeten@ugent.be>
2022-03-22 22:35:49 +0100

    you can easily give pages the property to lead to landing page if user not authorised

---
+++ frontend/src/components/FTotalRoutes.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {Route,Routes,BrowserRouter} from "react-router-dom";
+
+import UserService from '../services/user.service'
+
+import LandingPage from "../page_landing/LandingPage";
+import HomePage from "../page_home/HomePage";
+import LoginPage from "../page_login/LoginPage";
+import SignupPage from "../page_signup/SignupPage";
+import SettingsPage from "../page_settings/SettingsPage";
+import MyMoviesPage from "../page_mymovies/MyMoviesPage";
+import NotImplementedPage from "../page_notimplemented/NotImplementedPage";
+
+class FTotalRoutes extends Component {
+
+    constructor(probs) {
+        super(probs);
+        this.state = {logged:false}
+    }
+
+    check(page){
+        UserService.getAuthTest().then(x=>{
+            const logged = x!==undefined && x!=="failed"
+            if(logged!==this.state.logged)
+                this.setState({logged})
+        })
+        if(this.state.logged)
+            return page
+        else
+            return <LandingPage/>
+    }
+
+    render() {
+
+        return <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<LandingPage/>}/>
+                        <Route path="/home" element={this.check(<HomePage/>)}/>
+                        <Route path="/login" element={<LoginPage/>}/>
+                        <Route path="/signup" element={<SignupPage/>}/>
+                        <Route path="/settings" element={this.check(<SettingsPage/>)}/>
+                        <Route path="/mymovies" element={this.check(<MyMoviesPage/>)}/>
+                        <Route path="/notimplemented" element={<NotImplementedPage/>}/>
+                    </Routes>
+                </BrowserRouter>
+    }
+
+}
+export default FTotalRoutes
