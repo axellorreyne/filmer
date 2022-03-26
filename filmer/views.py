06e56b3f87cedf380e71caf1fe3f0a306db63b19
elnijs <Elias.Nijs@UGent.be>
2022-03-26 16:43:09 +0100

    fix error 500 page

--- filmer/views.py
+++ filmer/views.py
@@ -34,7 +34,7 @@ def custom_permission_denied_view(request, exception):
 def custom_page_not_found_view(request, exception):
     return render(request, '404.html', status=404)
 
-def custom_error_view(request, exception):
+def custom_error_view(request):
     return render(request, '500.html', status=500)
 
 
