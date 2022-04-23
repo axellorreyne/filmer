4d4b3e16f8dfb3bdc7dcab1529bfa6a23dd7604e
elnijs <Elias.Nijs@UGent.be>
2022-04-23 22:45:03 +0200

    solid login page frontend

--- frontend/src/page_landing/LandingPage.js
+++ frontend/src/page_landing/LandingPage.js
@@ -17,12 +17,20 @@ class LandingPage extends Component
     return (
 <div className="container h-100 d-flex flex-column align-items-center">
     <FHeaderAlt/>
-    <main className="mb-5 text-center">
-        <h1>Where people find their favourite movies!</h1>
-        <div className="mt-5">
-            <Link to="/signup"><button type="button" className="btn btn-primary m-1">Sign up</button></Link>
-            <Link to="/login"><button type="button" className="btn btn-light m-1">Log in</button></Link>
+    <main className="mb-5 mt-5 text-center">
+      <h1>Where people find their favourite movies!</h1>
+      <div className="mt-5 ffs-3 rgb-2">
+        <div className="mb-3">
+          <Link to="/signup"><button type="button" className="btn btn-primary m-1 col-10 col-md-2">Sign up</button></Link>
+          <Link to="/login"><button type="button" className="btn btn-light m-1 col-10 col-md-2">Log in</button></Link>
         </div>
+        <Link to="/solidlogin"><button type="button" className="btn btn-dark hover-bg-solid rgb-bg-solid rgb-1 m-1 col-10 col-md-2">
+          <div className="d-flex align-items-center justify-content-center">
+            <img className="me-1" src="https://genr.eu/wp/wp-content/uploads/2018/10/logo.svg" width="18px"/>
+            solid
+          </div>
+        </button></Link>
+      </div>
     </main>
     <FFooter/>
     <maintitle text="settings"/>
