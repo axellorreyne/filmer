87d693bd24d2cff81a96506af9c6c545a350e204
elnijs <Elias.Nijs@UGent.be>
2022-04-24 17:06:45 +0200

    settings page, check if data is valid

--- frontend/src/page_settings/SettingsPage.js
+++ frontend/src/page_settings/SettingsPage.js
@@ -9,6 +9,20 @@ import UserService from "../services/user.service";
 import FFooter from "../components/FFooter.js";
 import FHeader from "../components/FHeader";
 
+function passwordValidationTest(value)
+{
+  return((value.length >= 8 || value.length <= 40) ? undefined : 
+    (<div className="rgb-alert" role="alert"> 
+      The password must be between 8 and 40 characters. 
+    </div>));
+}
+
+function emailValidationTest(value)
+{
+  return((isEmail(value)) ? undefined : 
+    (<div className="rgb-alert" role="alert">This is not a valid email.</div>));
+}
+
 class Profile extends Component {
   
   constructor(props) 
@@ -30,6 +44,8 @@ class Profile extends Component {
       }
   } 
 
+  // TODO (Elias): get username and email from browser storage and update 
+  // browser storage when the patch request is succesfull
   componentDidMount() 
   {
     document.title = "Filmer: Settings";
@@ -57,6 +73,7 @@ class Profile extends Component {
     this.setState({password: button.target.value}) ;
   }
 
+
   patchUser(form)
   {
     form.preventDefault();
@@ -81,7 +98,7 @@ class Profile extends Component {
 2. [x] execute patch query
 3. [x] show placeholders
 4. [x] show response message on error
-5. [ ] check for valid data before sending request
+5. [x] check for valid data before sending request
 6. [ ] check old password before sending patch
 
 */
@@ -89,49 +106,49 @@ class Profile extends Component {
   render() {
     return (
 <div className="h-100 d-flex flex-column m-3 m-xxl-0">
-    <FHeader/>
-    <main className="mb-5 container-fluid">
-        <div className="my-5 d-lg-flex justify-content-around align-items-center">
-            <div className="col-lg-7 mx-md-5 mb-5" >
-                <p className="ffs-1 ffw-2 m-0 p-0 me-4">Settings</p>
-                <hr/> 
-                <Form onSubmit={this.patchUser} ref={form => this.form = form}>
-                  <div>
-                    <div className="mb-5">
-                        <div className="FForm d-lg-flex mb-3 align-items-center">
-                          <label className="form-label col-2 rgb-2 ffw-2" htmlFor="username">New Username</label>
-                          <Input type="text" className="FFormInput w-100" name="new-username" placeholder={this.state.currentUsername}
-                            value={this.state.username} onChange={this.buttonSetStateUsername}/>
-                        </div>
-                        <div className="FForm d-lg-flex mb-3 align-items-center">
-                          <label className="form-label col-2 rgb-2 ffw-2" htmlFor="email">New Email</label>
-                          <Input type="text" className="FFormInput w-100" name="new-email" placeholder={this.state.currentEmail}
-                            value={this.state.email} onChange={this.buttonSetStateEmail}/>
-                        </div>
-                        <div className="FForm d-lg-flex mb-3 align-items-center">
-                          <label className="form-label col-2 rgb-2 ffw-2" htmlFor="password">New Password</label>
-                          <Input type="password" className="FFormInput w-100" name="new-password" placeholder={this.state.currentPassword}
-                            value={this.state.password} onChange={this.buttonSetStatePassword}/>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                      <button className="btn btn-primary btn-block" disabled={this.state.loading} >
-                        {this.state.loading && <span className="spinner-border spinner-border-sm"/>}
-                        {!this.state.loading && <span>Save</span>}
-                      </button>
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <div className="py-2 rgb-alert" role="alert">
-                      {this.state.message}
-                    </div>
-                  </div>
-                  <CheckButton style={{ display: "none" }} ref={c => { this.checkBtn = c; }} />
-                </Form>
+  <FHeader/>
+  <main className="mb-5 container-fluid">
+    <div className="my-5 d-lg-flex justify-content-around align-items-center">
+      <div className="col-lg-7 mx-md-5 mb-5" >
+        <p className="ffs-1 ffw-2 m-0 p-0 me-4">Settings</p>
+        <hr/> 
+        <Form onSubmit={this.patchUser} ref={form => this.form = form}>
+          <div>
+            <div className="mb-5">
+              <div className="FForm d-lg-flex mb-3 align-items-center">
+                <label className="form-label col-2 rgb-2 ffw-2" htmlFor="username">New Username</label>
+                <Input type="text" className="FFormInput w-100" name="new-username" placeholder={this.state.currentUsername}
+                  value={this.state.username} onChange={this.buttonSetStateUsername}/>
+              </div>
+              <div className="FForm d-lg-flex mb-3 align-items-center">
+                <label className="form-label col-2 rgb-2 ffw-2" htmlFor="email">New Email</label>
+                <Input type="text" className="FFormInput w-100" name="new-email" placeholder={this.state.currentEmail}
+                  value={this.state.email} onChange={this.buttonSetStateEmail} validations={[emailValidationTest]}/>
+              </div>
+              <div className="FForm d-lg-flex mb-3 align-items-center">
+                <label className="form-label col-2 rgb-2 ffw-2" htmlFor="password">New Password</label>
+                <Input type="password" className="FFormInput w-100" name="new-password" placeholder={this.state.currentPassword}
+                  value={this.state.password} onChange={this.buttonSetStatePassword} validations={[passwordValidationTest]}/>
+              </div>
             </div>
-        </div>
-    </main>
-    <FFooter/>
+            <div className="form-group">
+              <button className="btn btn-primary btn-block" disabled={this.state.loading} >
+                  {this.state.loading && <span className="spinner-border spinner-border-sm"/>}
+                  {!this.state.loading && <span>Save</span>}
+              </button>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="py-2 rgb-alert" role="alert">
+              {this.state.message}
+            </div>
+          </div>
+          <CheckButton style={{ display: "none" }} ref={c => { this.checkBtn = c; }} />
+        </Form>
+      </div>
+    </div>
+  </main>
+  <FFooter/>
 </div>
         );
     }
