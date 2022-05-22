bee1826ca2f3951b4f4dcadd14a603d2fd8b80e7
Axel Lorreyne <Axel.Lorreyne@UGent.be>
2022-05-22 18:03:33 +0200

    postgres

--- backend/settings.py
+++ backend/settings.py
@@ -161,7 +161,7 @@ class Development(Base):
 
 class Production(Base):
     DATABASES = values.DatabaseURLValue(
-        "postgres://postgres:postgres@database:5432/filmer",
+        "postgres://postgres:postgres@postgres:5432/filmer",
         environ=False,
     )
 
