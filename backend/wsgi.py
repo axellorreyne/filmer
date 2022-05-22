27ff95c26452b8f462f521b74538e50f4dfbebba
Axel Lorreyne <Axel.Lorreyne@UGent.be>
2022-05-22 17:22:52 +0200

    fix database url

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -12,5 +12,6 @@ import os
 from configurations.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Production')
 
 application = get_wsgi_application()
