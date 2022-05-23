2e255f96a34ee36e8067a63afc54383a85266fae
elnijs <Elias.Nijs@UGent.be>
2022-05-24 00:24:22 +0200

    styling solid login

--- frontend/src/page_solidlogin/SolidLoginPage.js
+++ frontend/src/page_solidlogin/SolidLoginPage.js
@@ -6,13 +6,6 @@ import RsrcIconArrowLeft from "../resources/icon_arrow_left.svg";
 import FHeaderAlt from "../components/FHeaderAlt.js";
 import FFooter from "../components/FFooter.js";
 
-
-const required = value => {
-    if (!value) {
-        return (<div className="rgb-alert" role="alert">This field is required!</div>);
-    }
-};
-
 class SolidLogin extends Component {
 
     constructor(props) {
@@ -34,35 +27,33 @@ class SolidLogin extends Component {
                     <div className="d-lg-flex justify-content-around align-items-center">
                         <div className="col-lg-7">
                             <div className="mt-5 d-flex mb-3">
-                                <img src={RsrcIconArrowLeft} width="12px" className="me-2"/>
+                                <img src={RsrcIconArrowLeft} width="12px" className="me-2" alt=""/>
                                 <Link to="/">Back to landingpage</Link>
                             </div>
                             <h1>Solid Log In</h1>
                             <hr/>
                             <div className="mb-4">
                                 <div className="FForm d-lg-flex mb-3 align-items-baseline">
-                                    <label className="form-label col-2 rgb-2 ffw-2 " htmlFor="username">Solid
-                                        url*</label>
+                                    <label className="form-label col-2 rgb-2 ffw-2 " htmlFor="username">Solid url</label>
                                     <input type="text" className="FFormInput w-100" name="solidUrl"
                                            value={this.state.oidcIssuer} onChange={this.onChangeURL}/>
                                 </div>
                             </div>
-
-                            <LoginButton className="btn rgb-bg-solid rgb-1 btn-block"
-                                         disabled={this.state.loading}
-                                         authOptions={{clientName: "Filmer"}}
-                                         oidcIssuer={this.state.oidcIssuer}
-                                         redirectUrl={window.location.protocol + '//' + window.location.host + '/home'}
-                                         onError={console.error}>
-                                {this.state.loading && (<span className="spinner-border spinner-border-sm"/>)}
-                                <div className="d-flex align-items-center justify-content-center">
-                                    <img className="me-1" src="https://genr.eu/wp/wp-content/uploads/2018/10/logo.svg"
-                                         width="18px"/>
-                                    Continue
-                                </div>
-                            </LoginButton>
-
-
+                            
+                            <div className="btn rgb-bg-solid rgb-1 btn-block">
+                              <LoginButton disabled={this.state.loading}
+                                           authOptions={{clientName: "Filmer"}}
+                                           oidcIssuer={this.state.oidcIssuer}
+                                           redirectUrl={window.location.protocol + '//' + window.location.host + '/home'}
+                                           onError={console.error}>
+                                  {this.state.loading && (<span className="spinner-border spinner-border-sm"/>)}
+                                  <div className="d-flex align-items-center justify-content-center">
+                                      <img className="me-1" src="https://genr.eu/wp/wp-content/uploads/2018/10/logo.svg"
+                                           width="18px" alt=""/>
+                                      Continue
+                                  </div>
+                              </LoginButton>
+                            </div>
                             {this.state.message && (
                                 <div className="form-group">
                                     <div className="py-2 rgb-alert" role="alert">{this.state.message}</div>
